307ce53ad85214de57bf6b72a20d39f7daf56a06
AppTap Dev <apptap@sanisidro.es>
2026-04-07 09:11:49 +0200

    feat: redesign admin-scores panel with entrenamientos as primary section

--- src/app/features/admin/scores/admin-scores/admin-scores.component.ts
+++ src/app/features/admin/scores/admin-scores/admin-scores.component.ts
@@ -3,7 +3,9 @@ import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { CompeticionService } from '../../../scores/competicion.service';
+import { EntrenamientoService } from '../../entrenamientos/entrenamiento.service';
 import { Competicion } from '../../../../core/models/competicion.model';
+import { Entrenamiento } from '../../../../core/models/entrenamiento.model';
 
 @Component({
   selector: 'app-admin-scores',
@@ -14,20 +16,22 @@ import { Competicion } from '../../../../core/models/competicion.model';
 })
 export class AdminScoresComponent {
   private competicionService = inject(CompeticionService);
+  private entrenamientoService = inject(EntrenamientoService);
   private router = inject(Router);
 
   competiciones = toSignal(this.competicionService.getAll(), { initialValue: [] as Competicion[] });
+  entrenamientos = toSignal(this.entrenamientoService.getAll(), { initialValue: [] as Entrenamiento[] });
+
+  nuevoEntrenamiento(): void {
+    this.router.navigate(['/admin/entrenamientos/nuevo']);
+  }
+
+  verEntrenamiento(id: string): void {
+    this.router.navigate(['/admin/entrenamientos', id]);
+  }
 
   nuevaCompeticion(): void {
     this.router.navigate(['/admin/competiciones/nueva']);
-  }
-
-  nuevaEscuadra(): void {
-    this.router.navigate(['/admin/scores/escuadra/nueva']);
-  }
-
-  registrarResultados(): void {
-    this.router.navigate(['/admin/scores/resultados']);
   }
 
   totalPlatos(c: Competicion): number {
